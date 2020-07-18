1c0bef62a6dd577253beeffdd29f495fde0430b3
SouzaFabiano <67329184+fabi47@users.noreply.github.com>
2020-07-18 18:21:47 +0900

    update
    変更してみました。
    合っていますか？

--- kuku_new.php
+++ kuku_new.php
@@ -1,14 +1,29 @@
 <?php
 
-for ($i = 1; $i < 11; $i++) {
+for ($i = 1; $i < 10; $i++) {
 
-for ($a = 1; $a < 11; $a++) {
-
-    $fruit = array($i => $a);
-        foreach ($fruit as $key => $value) {
-            echo "[" . $key . "x" . $value . "=" . $i*$a . "]" . "\n";
+    $a = array('1' => $i , '2' => $i , '3' => $i , '4' => $i , '5' => $i , '6' => $i , '7' => $i , '8' => $i , '9' => $i);
+    foreach($a as $key => $value){
+        echo "(" , "$key" , "," , "$value" , "," , $key*$value , ")";
     }
 }
+
+
+
+
+/*---修正前---
+
+for ($i = 1; $i < 11; $i++) {
+
+    for ($a = 1; $a < 11; $a++) {
+
+        $fruit = array($i => $a);
+        foreach ($fruit as $key => $value) {
+            echo "[" . $key . "x" . $value . "=" . $i*$a . "]" . "\n";
+        }
+    }
 }
+
+---------*/
 
  ?>
