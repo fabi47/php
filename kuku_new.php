5658bcc0a8549bea40ee37a5a606d204c70e2ba0
fabi47 <fabi.47jsc@icloud.com>
2020-07-23 16:00:36 +0900

    function

--- kuku_new.php
+++ kuku_new.php
@@ -1,14 +1,38 @@
 <?php
-
+/*
 for ($i = 1; $i < 11; $i++) {
 
 for ($a = 1; $a < 11; $a++) {
+*/
+for ($i = 1; $i < 10; $i++){
 
-    $fruit = array($i => $a);
-        foreach ($fruit as $key => $value) {
-            echo "[" . $key . "x" . $value . "=" . $i*$a . "]" . "\n";
-    }
+    $info = array(
+            "1の段" =>  array(
+                "1" * "$i"
+            ),
+            "1の段" =>  array(
+                "1" * "$i"
+            )
+            
+
+            foreach ($info as $values) {
+                foreach ($values as $value) {
+                    echo $value . "\n";
+                }
+            }
+}
+
+
+
+
+
+
+            /*
+    foreach ($kuku as $key => $value) {
+        echo "[" . $key . "x" . $value . "=" . $i*$a . "]" . "\n";
+}*/
+/*
 }
 }
-
+*/
  ?>
--- testfile.php
+++ testfile.php
@@ -1,3 +1,28 @@
 <?php
-echo "hello";
+
+for ($i = 1; $i < 10; $i++){
+
+    $info = array(
+            "1の段" =>  array(
+                "1" * "$i"
+
+            ),
+            "kuku" =>  array(
+                "2" * $i,
+                "3" * $i,
+                "4" * $i,
+                "5" * $i,
+                "6" * $i,
+                "7" * $i,
+                "8" * $i,
+                "9" * $i
+            )
+            );
+
+    foreach ($info as $values) {
+        foreach ($values as $value) {
+            echo $value . "\n";
+        }
+    }
+}
  ?>
